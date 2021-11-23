0c63ab1b6d29ea94a23d465067de910d375509fc
faithm2020 <91081126+faithm2020@users.noreply.github.com>
2021-11-23 05:03:26 +0400

    Lab3

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
 
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
